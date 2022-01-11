cc77b47fa5059f1218cae737d908b8607ef1437e
Ansumane Mané <manoansu@gmail.com>
2022-01-11 10:20:25 +0000

    Movie mobile with css implementad

--- frontend/src/page/Member/Movies/Form/index.tsx
+++ frontend/src/page/Member/Movies/Form/index.tsx
@@ -1,8 +1,29 @@
 import './styles.css';
+import FilmeIcon from '../../../../assets/images/filme.png';
+
+
 const Form = () =>{
 
     return(
-        <h1>hello</h1>
+        <div className="movie-detalhe-container">
+            <div className="movie-select-container">
+                <h1>Select</h1>
+            </div>
+            <div className="movie-content-container">
+                <div className="movie-image-container">
+                    <img src={FilmeIcon} alt="filme" />
+                </div>
+                <div className="movie-title-container">
+                    <h2>O Retorno do Rei</h2>
+                </div>
+                <div className="movie-year-container">
+                    <h4>2013</h4>
+                </div>
+                <div className="movie-legenda-container">
+                    <h6>O olho do inimigo está se movendo.</h6>
+                </div>
+            </div>
+        </div>
     );
 };
 
--- frontend/src/page/Member/index.tsx
+++ frontend/src/page/Member/index.tsx
@@ -1,7 +1,8 @@
 import { AuthContext } from 'AuthContext';
 import { useContext, useEffect } from 'react';
-import { Link, Redirect } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import { getTokenData, isAuthenticated } from 'util/auth';
+import Form from './Movies/Form';
 import './styles.css';
 
 
@@ -26,25 +27,13 @@ const Member = () =>{
 
     return (
         <div className="movie-container">
-            <div className="movie-content">
-                <h1>Tela listagem de filmes</h1>
-                { authContextData.authenticated ? (
-                <>
-                    <Link to="/movies/1">
-                        <div className="movie-content-link">
-                            <h6>Acessar/movies/1</h6>
-                        </div>
-                    </Link>
-                    <Link to="/movies/2">
-                        <div className="movie-content-link-2">
-                            <h6>Acessar/movies/2</h6>
-                        </div>
-                    </Link>
-                </>
-                ) : (
-                    <Redirect to="/" />
-                )}
-            </div>
+            
+            { authContextData.authenticated ? (
+                <Form />
+            ) : (
+                <Redirect to="/" />
+            )}
+            
         </div>
     );
 };
